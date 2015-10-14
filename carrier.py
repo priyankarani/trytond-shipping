dd4e23cf5c818c6cd34ed458da1737b94c123b5d
Priyanka Rani <priyanka.rani@fulfil.io>
2015-10-13 22:46:09 -0500

    Implement carrier service common for all shipping modules

--- carrier.py
+++ carrier.py
@@ -4,10 +4,11 @@
 
 """
 from trytond.pool import PoolMeta, Pool
-from trytond.model import fields
+from trytond.pyson import Eval, Bool
+from trytond.model import fields, ModelSQL, ModelView
 from trytond.transaction import Transaction
 
-__all__ = ['Carrier']
+__all__ = ['Carrier', 'CarrierService']
 __metaclass__ = PoolMeta
 
 
@@ -41,3 +42,47 @@ class Carrier:
         # TODO: Remove this method in next version and use `get_shipping_rates`
         # method in sale instead
         return []  # pragma: no cover
+
+
+class CarrierService(ModelSQL, ModelView):
+    "Carrier Service"
+    __name__ = 'carrier.service'
+
+    active = fields.Boolean('Active', select=True)
+    name = fields.Char('Name', required=True, select=True, readonly=True)
+    value = fields.Char('Value', required=True, select=True, readonly=True)
+    method_type = fields.Selection(
+        'get_method_type', 'Type', select=True, readonly=True,
+        states={'required': Bool(Eval('is_method_type_required'))},
+        depends=['is_method_type_required']
+    )
+    display_name = fields.Char('Display Name', select=True)
+    source = fields.Selection(
+        'get_source', 'Source', required=True, readonly=True
+    )
+
+    is_method_type_required = fields.Function(
+        fields.Boolean("Is Method Type Required ?"),
+        'on_change_with_is_method_type_required'
+    )
+
+    def on_change_with_is_method_type_required(self, name=None):
+        return True
+
+    @classmethod
+    def get_method_type(cls):
+        """
+        Get the method type
+        """
+        return [(None, '')]
+
+    @classmethod
+    def get_source(cls):
+        """
+        Get the source
+        """
+        return []
+
+    @staticmethod
+    def default_active():
+        return True
