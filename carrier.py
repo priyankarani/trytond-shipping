17003e8b5af0e1b3e96633b96abb06367c286360
Priyanka Rani <priyanka.rani@fulfil.io>
2015-10-24 18:43:43 -0500

    Allow currency to be hidden for carriers which do not respect them

--- carrier.py
+++ carrier.py
@@ -6,6 +6,8 @@
 from trytond.pool import PoolMeta, Pool
 from trytond.model import fields
 from trytond.transaction import Transaction
+from trytond import backend
+from trytond.pyson import Eval, Bool
 
 __all__ = ['Carrier']
 __metaclass__ = PoolMeta
@@ -15,15 +17,51 @@ class Carrier:
     "Carrier"
     __name__ = 'carrier'
 
-    currency = fields.Many2One('currency.currency', 'Currency', required=True)
+    currency = fields.Function(
+        fields.Many2One(
+            'currency.currency', 'Currency', states={
+                'invisible': Bool(Eval('hide_currency'))
+            }, depends=['hide_currency']
+        ), 'get_currency'
+    )
+    hide_currency = fields.Function(
+        fields.Boolean("Hide Currency ?"), 'on_change_with_hide_currency'
+    )
 
-    @staticmethod
-    def default_currency():
+    @fields.depends('carrier_cost_method')
+    def on_change_with_hide_currency(self, name=None):
+        return self._get_hide_currency()
+
+    def _get_hide_currency(self):
+        """
+        Downstream module can implement this method to hide or show currency
+        field accordingly
+        """
+        return True
+
+    @classmethod
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        cursor = Transaction().cursor
+
+        super(Carrier, cls).__register__(module_name)
+
+        table = TableHandler(cursor, cls, module_name)
+
+        # Remove currency field
+        if table.column_exist('currency'):
+            table.drop_column('currency')
+
+    def get_currency(self, name):
+        """
+        Downstream module can implement this method to return respective
+        currency
+        """
         Company = Pool().get('company.company')
 
-        company = Transaction().context.get('company')
-        if company:
-            return Company(company).currency.id
+        company_id = Transaction().context.get('company')
+        if company_id:
+            return Company(company_id).currency.id
 
     def get_rates(self):
         """
